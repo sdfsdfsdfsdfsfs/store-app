518d1075603e0d72f38d98084c88048bdf6d711b
Daniel Sanchez <danxschz@gmail.com>
2022-07-07 15:57:42 -0400

    Initial commit

---
+++ src/components/Shop.js
@@ -0,0 +1,15 @@
+const Shop = () => {
+  return (
+    <div className="items">
+      <div class="item">
+        <img src="https://cdn-image02.casetify.com/usr/25496/1195496/~v119/3558186_iphone13-pro__color_blue_16003733.png.350x350-w.m80.jpg" alt="case"/>
+        <div>iPhone 13 Pro</div>
+        <div>Naruto</div>
+        <div>Sasuke and Naruto</div>
+        <div>$20 USD</div>
+      </div>
+    </div>
+  )
+}
+
+export default Shop;
