27e4cb34ad684d4f7f1e67856d6c9a4d5c7f95a6
Daniel Sanchez <danxschz@gmail.com>
2022-07-08 20:55:25 -0400

    Build and style InfoBar component

---
+++ src/components/InfoBar.js
@@ -0,0 +1,13 @@
+import '../styles/InfoBar.css';
+
+const InfoBar = () => {
+  return (
+    <div className="info-bar">
+      <strong>Free Shipping!</strong>
+      <span>VE Orders <strong>$50+</strong></span>
+      <span>Outside VE <strong>$100+</strong></span>
+    </div>
+  )
+}
+
+export default InfoBar;
