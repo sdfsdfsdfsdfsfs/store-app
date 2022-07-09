8c6ee5b87332b2a7621703273778c427fbedf6ea
Daniel Sanchez <danxschz@gmail.com>
2022-07-09 01:41:52 -0400

    Add more content to InfoBar and update style

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import Header from './components/Header';
+import InfoBar from './components/InfoBar';
 import Shop from './components/Shop';
 
 function App() {
   return (
     <div className="App">
+      <InfoBar />
+      <Header />
       <Shop />
     </div>
   );
--- src/components/InfoBar.js
+++ src/components/InfoBar.js
@@ -3,9 +3,21 @@ import '../styles/InfoBar.css';
 const InfoBar = () => {
   return (
     <div className="info-bar">
-      <strong>Free Shipping!</strong>
-      <span>VE Orders <strong>$50+</strong></span>
-      <span>Outside VE <strong>$100+</strong></span>
+      <div className="info-bar__socials">
+        <a href="https://instagram.com" target="_blank" rel="noreferrer"><i className="fa-brands fa-instagram"></i></a>
+        <a href="https://facebook.com" target="_blank" rel="noreferrer"><i class="fa-brands fa-facebook"></i></a>
+        <a href="https://tiktok.com" target="_blank" rel="noreferrer"><i class="fa-brands fa-tiktok"></i></a>
+      </div>
+      <div className="info-bar__shipping">
+        <strong>Free Shipping!</strong>
+        <span>VE Orders <strong>$50+</strong></span>
+        <span>Outside VE <strong>$100+</strong></span>
+      </div>
+      <div className="hidden">
+        <i className="fa-brands fa-instagram"></i>
+        <i class="fa-brands fa-facebook"></i>
+        <i class="fa-brands fa-tiktok"></i>
+      </div>
     </div>
   )
 }
