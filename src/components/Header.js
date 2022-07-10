8737e710386161af063e2afc92d1809729863a1b
Daniel Sanchez <danxschz@gmail.com>
2022-07-10 02:30:13 -0400

    Add hover styles to infobar and header

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,13 +9,13 @@ const Header = () => {
       <div className="logo">The Rage Lab</div>
       <nav>
         <ul>
-          <li>Cases</li>
-          <li>Collections</li>
-          <li>Accesories</li>
-          <li>About Us</li>
+          <li className="nav-item">Cases</li>
+          <li className="nav-item">Collections</li>
+          <li className="nav-item">Accesories</li>
+          <li className="nav-item">About Us</li>
         </ul>
       </nav>
-      <div className="header_icons">
+      <div className="header__icons">
         <div className="search-form">
           <input type="search" id="search" placeholder="Search..." onFocus={setFocusStyle} onBlur={removeFocusStyle}></input>
           <button type="button"><i className="fa-solid fa-magnifying-glass"></i></button>
