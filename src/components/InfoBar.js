0c92e173b7071a6c516786c911c6998cf9d39071
Daniel Sanchez <danxschz@gmail.com>
2022-07-12 00:57:05 -0400

    Rewrite infobar style in scss

--- src/components/InfoBar.js
+++ src/components/InfoBar.js
@@ -1,14 +1,14 @@
-import '../styles/InfoBar.css';
+import '../styles/InfoBar.scss';
 
 const InfoBar = () => {
   return (
     <div className="info-bar">
       <div className="info-bar__socials">
-        <a href="https://instagram.com" target="_blank" rel="noreferrer"><i className="fa-brands fa-instagram"></i></a>
-        <a href="https://facebook.com" target="_blank" rel="noreferrer"><i class="fa-brands fa-facebook"></i></a>
-        <a href="https://tiktok.com" target="_blank" rel="noreferrer"><i class="fa-brands fa-tiktok"></i></a>
+        <a href="https://instagram.com" target="_blank" rel="noreferrer" className="hvr-grow"><i className="fa-brands fa-instagram"></i></a>
+        <a href="https://facebook.com" target="_blank" rel="noreferrer" className="hvr-grow"><i class="fa-brands fa-facebook"></i></a>
+        <a href="https://tiktok.com" target="_blank" rel="noreferrer" className="hvr-grow"><i class="fa-brands fa-tiktok"></i></a>
       </div>
-      <div className="info-bar__shipping">
+      <div className="info-bar__info">
         <strong>Free Shipping!</strong>
         <span>VE Orders <strong>$50+</strong></span>
         <span>Outside VE <strong>$100+</strong></span>
