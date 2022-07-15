d59aff1d06767deeb23aea26efc747dbc2efbba2
Daniel Sanchez <danxschz@gmail.com>
2022-07-15 01:05:57 -0400

    Reorganize case images and data

---
+++ src/data/cases.js
@@ -0,0 +1,47 @@
+import whiteClouds from '../assets/images/cases/white-clouds.png';
+import giornoPattern from '../assets/images/cases/giorno-pattern.png';
+import luffy from '../assets/images/cases/luffy.png';
+import noFace from '../assets/images/cases/no-face.png';
+import nezukoPattern from '../assets/images/cases/nezuko-pattern.png';
+import sharingan from '../assets/images/cases/sharingan.png';
+
+const cases = [
+  {
+    collection: 'Naruto',
+    name: 'White Clouds',
+    price: 20,
+    img: whiteClouds,
+  },
+  {
+    collection: "Jojo's Bizarre Adventures",
+    name: 'Giorno Pattern',
+    price: 20,
+    img: giornoPattern,
+  },
+  {
+    collection: 'One Piece',
+    name: 'Luffy',
+    price: 20,
+    img: luffy,
+  },
+  {
+    collection: 'Spirited Away',
+    name: 'No Face',
+    price: 20,
+    img: noFace,
+  },
+  {
+    collection: 'Demon Slayer',
+    name: 'Nezuko Pattern',
+    price: 20,
+    img: nezukoPattern,
+  },
+  {
+    collection: 'Naruto',
+    name: 'Sharingan',
+    price: 20,
+    img: sharingan,
+  }
+]
+
+export default cases;
