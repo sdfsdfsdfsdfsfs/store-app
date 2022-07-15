0f5011491d7440fe24867847c72e8f654f73fd0d
Daniel Sanchez <danxschz@gmail.com>
2022-07-15 01:06:43 -0400

    Refactor Item component

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,16 +1,16 @@
-import '../styles/Item.css';
+import './styles/Item.scss';
 
 const Item = (props) => {
-  const { img, collection, model, price } = props;
+  const { img, collection, name, price } = props.case;
 
   return (
     <div className="item">
-      <img src={img} alt={`${model} case`}/>
+      <img src={img} alt={`${name} case`}/>
       <div className="item__collection">{collection}</div>
-      <div>{model}</div>
+      <div className="item__name">{name}</div>
       <div className="item__price">{`$${price} USD`}</div>
     </div>
   )
 }
 
-export default Item
+export default Item;
